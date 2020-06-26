27596fddc16cf1bcbc0741e8c04b3a3159559bee
Philip Dubois <hello@philipdubois.be>
2020-06-26 14:12:10 +0200

    Add 18.04 and 20.04

--- manifests/params.pp
+++ manifests/params.pp
@@ -24,7 +24,7 @@ class icinga2::params {
       $package_provider = 'yum'
     }
 
-	#RedHat systems:
+  #RedHat systems:
     'RedHat': {
       #Pick the right package provider:
       $package_provider = 'yum'
@@ -121,6 +121,20 @@ class icinga2::params {
           $server_plugin_package_install_options = '--no-install-recommends'
         }
         '16.04': {
+          $icinga2_server_package = 'icinga2'
+          $icinga2_server_plugin_packages = [ 'nagios-plugins', 'nagios-plugins-basic', 'nagios-plugins-common', 'nagios-plugins-standard', 'nagios-snmp-plugins', 'nagios-plugins-extra', 'nagios-plugins-contrib', 'nagios-nrpe-plugin']
+          $icinga2_server_mail_package = 'mailutils'
+          #Specify '--no-install-recommends' so we don't inadvertently get Nagios 3 installed; it comes as a recommended package with most of the plugin packages:
+          $server_plugin_package_install_options = '--no-install-recommends'
+        }
+        '18.04': {
+          $icinga2_server_package = 'icinga2'
+          $icinga2_server_plugin_packages = [ 'nagios-plugins', 'nagios-plugins-basic', 'nagios-plugins-common', 'nagios-plugins-standard', 'nagios-snmp-plugins', 'nagios-plugins-extra', 'nagios-plugins-contrib', 'nagios-nrpe-plugin']
+          $icinga2_server_mail_package = 'mailutils'
+          #Specify '--no-install-recommends' so we don't inadvertently get Nagios 3 installed; it comes as a recommended package with most of the plugin packages:
+          $server_plugin_package_install_options = '--no-install-recommends'
+        }
+        '20.04': {
           $icinga2_server_package = 'icinga2'
           $icinga2_server_plugin_packages = [ 'nagios-plugins', 'nagios-plugins-basic', 'nagios-plugins-common', 'nagios-plugins-standard', 'nagios-snmp-plugins', 'nagios-plugins-extra', 'nagios-plugins-contrib', 'nagios-nrpe-plugin']
           $icinga2_server_mail_package = 'mailutils'
